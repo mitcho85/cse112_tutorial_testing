dec4e7ee203d68dc94e02a9237a2a80f68db4f02
mitcho85 <mitopete@ucsd.edu>
2017-04-18 12:58:22 -0700

    Test build 2

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 var assert = require('assert');
+var x = 10;
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
